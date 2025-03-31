b4308f35399bca6982667ee19652314b9b9cac1b
abderahman <abderahman.mzd@gmail.com>
2025-03-31 10:43:04 +0000

    Initial commit: Set up Spring Boot project

---
+++ src/main/java/com/example/career_goals_tracker/CareerGoalsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.career_goals_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerGoalsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerGoalsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/career_goals_tracker/CareerGoalsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.career_goals_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGoalsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
